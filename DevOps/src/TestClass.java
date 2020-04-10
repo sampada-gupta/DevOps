2c0b4344e318173c11d081f4ac2b33bdc86afc05
sampada-gupta <62881938+sampada-gupta@users.noreply.github.com>
2020-04-10 14:16:07 +0530

    Updating comments for github on local machine

--- DevOps/src/TestClass.java
+++ DevOps/src/TestClass.java
@@ -2,7 +2,8 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Testi class created for DevOps");
+		System.out.println("Testing class created for DevOps");
+		System.out.println("Testing class created for GitHub to be created");
 
 	}
 
